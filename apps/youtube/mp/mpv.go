c754ced185a35d60fe974e6ccc7219466f9a9558
gvsro <vlad@redcodenetwork.ro>
2016-12-16 01:19:32 +0200

    test

--- apps/youtube/mp/mpv.go
+++ apps/youtube/mp/mpv.go
@@ -22,7 +22,7 @@ import (
 	"strings"
 	"sync"
 	"time"
-
+	"github.com/googollee/go-socket.io"
 	"github.com/gvsro/plaincast/config"
 	"github.com/gvsro/plaincast/log"
 )
@@ -45,6 +45,31 @@ func (mpv *MPV) initialize() (chan State, int) {
 	if mpv.handle != nil || mpv.running {
 		panic("already initialized")
 	}
+	
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+			log.Fatal(err)
+	}
+	server.On("connection", func(so socketio.Socket) {
+			log.Println("on connection")
+			so.Join("chat")
+			so.On("chat message", func(msg string) {
+					log.Println("emit:", so.Emit("chat message", msg))
+					so.BroadcastTo("chat", "chat message", msg)
+			})
+			so.On("disconnection", func() {
+					log.Println("on disconnect")
+			})
+	})
+	server.On("error", func(so socketio.Socket, err error) {
+			log.Println("error:", err)
+	})
+
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./asset")))
+	log.Println("Serving at localhost:5000...")
+	log.Fatal(http.ListenAndServe(":5000", nil))
+
 
 	mpv.mainloopExit = make(chan struct{})
 	mpv.running = true
