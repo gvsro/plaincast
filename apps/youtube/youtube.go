419784d61873ada037f4dedf6b73b977dc355004
gvsro <vlad@redcodenetwork.ro>
2016-12-16 14:52:31 +0200

    made changes

--- apps/youtube/youtube.go
+++ apps/youtube/youtube.go
@@ -96,7 +96,7 @@ type outgoingMessage struct {
 
 // New returns a new YouTube object (app).
 func New(systemName string) *YouTube {
-	logger.Println(skp)
+	logger.Println(socketplayer)
 	yt := YouTube{}
 	yt.systemName = systemName
 	yt.runQuit = make(chan struct{})
